dc2912415f514b653d07e887c428745252f73099
quzhengyang <quzhengyang@xiaoyezi.com>
2022-01-06 14:40:17 +0800

    composer packagelist test v1.1

--- src/hello.php
+++ src/hello.php
@@ -3,11 +3,11 @@ namespace qzy\hello;
 
 class Hello {
 
-//    public function __construct() {
-//        echo "qzy\hello has load";
-//    }
+    public function __construct() {
+        echo "qzy\hello has load";
+    }
 
-    public function say($p) {
+    public static function say($p) {
 
         return "hello world.  $p";
     }
