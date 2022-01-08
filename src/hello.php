8aea56425384ec2bc81d71f4edfb58c4dbaaac43
quzhengyang <quzhengyang@xiaoyezi.com>
2022-01-08 18:37:43 +0800

    TOERP-5035: [feature] confCilent

--- src/hello.php
+++ src/hello.php
@@ -3,10 +3,6 @@ namespace qzy\hello;
 
 class Hello {
 
-    public function __construct() {
-        echo "qzy\hello has load";
-    }
-
     public static function say($p) {
 
         return "hello world.  $p";
